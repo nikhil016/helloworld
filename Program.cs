6175bfb87b5cfd6eef744e03f32d1137b2ba3f82
Nikhil Rathi <nikhil.rathi@keysight.com>
2020-09-20 22:18:24 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            for (i=0; i<10;i++)
+            {
+                Console.WriteLine("This is testing for git and jenkins"+i);
+             
+            }
+            Console.ReadKey();
+        }
+    }
+}
